b23f5a07fbd95d7ec68047f6b303bcc47ae3d391
ProgrammerPoltorashka <msargsyan2002@outlook.com>
2021-09-24 13:26:12 +0300

    Employee can see his orders in cards in Employee panel

--- Workplaces/Controllers/OrdersController.cs
+++ Workplaces/Controllers/OrdersController.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using Workplaces.Models;
 using Workplaces.Service;
 using System.Security.Claims;
-
+using System.Data.Entity;
 
 namespace Workplaces.Controllers
 {
     public class OrdersController : Controller
     {
+        public static DateTime Orderdate = new DateTime();
         private readonly AppDbContext _context;
         private readonly IWorkPlacesService _workPlaces;
 
@@ -25,8 +26,9 @@ namespace Workplaces.Controllers
         {  
             ViewBag.Orders = _context.Orders.ToList();
             ViewBag.Items = _context.Items.ToList();
-            ViewBag.Date = date;
+            ViewBag.CorrentUserOrders = _context.Orders.Where(order => order.UserId == GetCorrentUserId()).ToList();
 
+            Orderdate = date;
             List<Workplace> workplaces = _context.Workplaces.ToList();
             foreach(Orders orders in _context.Orders.ToList())
             {
@@ -39,18 +41,32 @@ namespace Workplaces.Controllers
 
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Order(int? id, DateTime date)
+        public IActionResult OrderTable(int? id)
         {
-            ViewBag.Users = _context.Users.ToList();
-            ViewBag.CorrentUserId = GetCorrentUserId();
+            ViewBag.d = Orderdate;
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var workplace = _context.Workplaces.Find(id);
+            if (workplace == null)
+            {
+                return NotFound();
+            }
+
+            return View(workplace);
+        }
+
+        [HttpPost, ActionName("OrderTable")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OrderConfirmed(int id)
+        {
             if (ModelState.IsValid)
             {
                 Orders order = new Orders()
                 {
                     UserId = GetCorrentUserId(),
-                    Date = date,
+                    Date = Orderdate,
                     WorkPlaceId = id
                 };
                 _context.Orders.Add(order);
@@ -58,7 +74,29 @@ namespace Workplaces.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return RedirectToAction("Index");
+
         }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Order(int? id, DateTime date)
+        //{
+        //    ViewBag.Users = _context.Users.ToList();
+        //    ViewBag.CorrentUserId = GetCorrentUserId();
+        //    if (ModelState.IsValid)
+        //    {
+        //        Orders order = new Orders()
+        //        {
+        //            UserId = GetCorrentUserId(),
+        //            Date = date,
+        //            WorkPlaceId = id
+        //        };
+        //        _context.Orders.Add(order);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
         public int GetCorrentUserId() 
         {
